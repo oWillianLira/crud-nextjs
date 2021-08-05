71813010aa0bf81cf8863970f60249affc28af83
Willian Lira <will.lirag3@gmail.com>
2021-08-04 21:49:57 -0300

    to vercel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,16 +48,7 @@ export default function Home() {
           <>
             <div className="flex justify-end">
               <Button className="mb-4 flex" color="green" onClick={newClient}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
-                </svg>
-                New Player
+                + New Player
               </Button>
             </div>
             <Table clients={clients} selectedClient={selectedClient} deletedClient={deletedClient} />
